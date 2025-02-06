9d1efefc4420c7ea3bcb106ee1b57accbef6f4f7
gitnassima20 <nassimabaroudi8@gmail.com>
2025-02-06 20:20:55 +0100

    Declared variables

--- go/1.hello-world.go
+++ go/1.hello-world.go
@@ -3,4 +3,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("Starting Textio Server")
+	fmt.Printf("%v %f %v %q\n",smsSendingLimit, costPerSMS, hasPermission, username)
 }
---
+++ go/2.variables.go
@@ -0,0 +1,7 @@
+package main
+
+var smsSendingLimit int
+var costPerSMS float64
+var hasPermission bool
+var username string
+
