0ce9a0291b12daf94f932653af087905642627de
gitnassima20 <nassimabaroudi8@gmail.com>
2025-02-08 21:43:47 +0100

    Added computed const example

--- go/2.variables.go
+++ go/2.variables.go
@@ -20,6 +20,11 @@ func printVariables() {
     //constants declaration: doesn't support short declaration syntax
     const premiumPlanName = "Premmium Plan"
 
+	//computed constans
+	const secondInMinute = 60
+	const minutesInHour = 60
+	const secondInHour = secondInMinute * minutesInHour
+
 	// Print values
 	fmt.Println("Total Messages:", totalMessages)
 	fmt.Println("Text Message:", textMsg)
@@ -28,5 +33,7 @@ func printVariables() {
     fmt.Println(averageOpenRate, displayMessage)
 
     fmt.Println("plan:", premiumPlanName)
+
+	fmt.Println("number of seconds in an hour:", secondInHour)
 }
 
