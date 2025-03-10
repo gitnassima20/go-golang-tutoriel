6c6763e1317e010db0c94eec51b2f6d9150120e9
gitnassima20 <nassimabaroudi8@gmail.com>
2025-03-10 14:03:42 +0000

    Added a simple example of error handling

--- go/9-errors.go
+++ go/9-errors.go
@@ -2,18 +2,23 @@ package main
 
 import "fmt"
 
-func sendSMSToCouple(msgToCustomer, msgToSpouse string) (int, error) {
-	msgCus, msgSpo, err := sendSMS(msgToCustomer), sendSMS(msgToSpouse)
+func sendSMSToCouple(msgToCustomer, msgToSpouse string) (float64, error) {
+	costSMSCustomer, err := sendSMS(msgToCustomer)
 	if err != nil {
-		fmt.Println(err)
+		return 0.0, err
 	}
+	costSMSSpouse, err := sendSMS(msgToSpouse)
+	if err != nil {
+		return 0.0, err
+	}
+	return costSMSCustomer + costSMSSpouse, nil
 }
 
-func sendSMS(message string) (int, error) {
+func sendSMS(message string) (float64, error) {
 	const maxTextLen = 25
 	const costPerChar = 2
 	if len(message) > maxTextLen {
 		return 0, fmt.Errorf("can't send texts over %v characters", maxTextLen)
 	}
-	return costPerChar * len(message), nil
+	return costPerChar * float64(len(message)), nil
 }
